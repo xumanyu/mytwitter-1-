3881d241894c78ed34324b61ad485fb147cdd79b
xumanyu <841557712@qq.com>
2015-12-03 02:26:41 +0000

    WSP Project-2015

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  end  
+  def new
+  	@user = User.new
+  end
+  def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+          remember @user
+        	 flash[:success] = "Welcome to the Twitter App!"
+           redirect_to @user# Handle a successful save.
+        else
+            render 'new'# Handle an unsuccessful save.     
+        end
+ end
+end
---
+++ app/models/micropost.rb
@@ -0,0 +1,6 @@
+class Micropost < ActiveRecord::Base
+      belongs_to :user    # Association with User
+
+      validates :user_id, presence: true
+      validates :content, presence: true, length: { maximum: 140 }
+    end
